4c3300a589da241bbff10a1a6fdec6732b0fad91
Wayne Hsiao <xiao.x.wayne.-nd@disney.com>
2017-04-02 18:18:33 +0800

    Add framework

---
+++ Carthage/Build/iOS/demo.framework/Headers/demo.h
@@ -0,0 +1,19 @@
+//
+//  demo.h
+//  demo
+//
+//  Created by Hsiao, Wayne Yu-Hann on 02/04/2017.
+//  Copyright © 2017 com.wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for demo.
+FOUNDATION_EXPORT double demoVersionNumber;
+
+//! Project version string for demo.
+FOUNDATION_EXPORT const unsigned char demoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demo/PublicHeader.h>
+
+
